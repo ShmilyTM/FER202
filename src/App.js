594df9e8d3f7b1c8ec482caa5250c9efddebfcba
ShmilyTM <lethienacqt@gmail.com>
2025-01-09 00:10:06 +0700

    Ex4

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CourseList from "./CourseList"; // Import component
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <CourseList />
+    </div>
+  );
+};
+
+export default App;
