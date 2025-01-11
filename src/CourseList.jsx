c0f3bcfcc1e30c91ce678edc6d9a61198c071392
ShmilyTM <lethienacqt@gmail.com>
2025-01-11 23:23:18 +0700

    add bt12345

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import "./App.css";
 
-const CourseList = () => {
+//Course Name
+/*const CourseList = () => {
   const courses = ["React", "ReactNative", "NodeJs"]; 
 
   return (
@@ -14,5 +16,54 @@ const CourseList = () => {
     </div>
   );
 };
+//Hello React
+*/ 
+/*const CourseList = () => {
+  return (
+    <h1>
+      Hello <span style={{ color: "blue" }}>React</span>
+    </h1>
+  );
+}; */
 
+//display images
+/*const CourseList = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+        alt="React Logo"
+        style={{ width: "150px" }}
+      />
+      <h2>
+         <span style={{ color: "blue" }}>This is the react logo</span>
+      </h2>
+      
+    </div>
+  );
+};*/
+//Navbar
+/*const CourseList = () => {
+  return (
+    <nav style={{ display: "flex", backgroundColor: "#333", padding: "10px" }}>
+      <a href="#" style={{ color: "white", margin: "0 15px" }}>
+        Home
+      </a>
+      <a href="#" style={{ color: "white", margin: "0 15px" }}>
+        Search
+      </a>
+      <a href="#" style={{ color: "white", margin: "0 15px" }}>
+        Contact
+      </a>
+      <a href="#" style={{ color: "white", margin: "0 15px" }}>
+        Login
+      </a>
+    </nav>
+  );
+};*/
+const CourseList = () => {
+  return (
+    <h1 style={{ color: "blue", fontWeight: "bold" }}>This is JSX</h1>
+  );
+};
 export default CourseList;
